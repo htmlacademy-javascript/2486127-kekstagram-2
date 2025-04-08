dab37c96382d62396315347e4410638d910356cf
daria-kretova <kretovadar@yandex.ru>
2025-04-08 22:24:03 +0300

    check-lint

--- js/functions.js
+++ js/functions.js
@@ -122,7 +122,7 @@ const checkMeeting = (startWork, endWork, startMeeting, duration) => {
   const getMinutes = (time) => {
     const [hours, minutes] = time.split(':').map(Number);
     return hours * 60 + minutes;
-  }
+  };
 
   const workStart = getMinutes(startWork);
   const workEnd = getMinutes(endWork);
@@ -130,7 +130,7 @@ const checkMeeting = (startWork, endWork, startMeeting, duration) => {
   const meetingEnd = meetingStart + duration;
 
   return meetingStart >= workStart && meetingEnd <= workEnd;
-}
+};
 
 window.console.log(checkMeeting('08:00', '17:30', '14:00', 90));
 window.console.log(checkMeeting('8:0', '10:0', '8:0', 120));
